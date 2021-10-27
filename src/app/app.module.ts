f528767bf2718758147918ed64acab3907c06e56
Jamilett <jam.letsam93@gmail.com>
2021-10-26 19:45:32 -0500

    Primer servicio heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
+import { HeroesService } from './services/heroes.service';
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -25,7 +26,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService], // En providers van todos los servicios
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+// Servicio
+import { HeroesService, Heroe } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styles: [
-  ]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  /** --------------------- PROPIEDADES --------------------- */
+  // Propiedad inicializada como un array que guardará los datos obtenidos desde el servicio
+  heroes: Heroe[] = [];
+
+  /** --------------------- SERVICIOS --------------------- */
+  // El servicio se llama dentro del constructor del componente
+  // Estructura: scope _nombreServicio: type
+  constructor(private _heroesService: HeroesService) {
+    console.log("Constructor")
+  }
 
   ngOnInit(): void {
+    // Se obtiene la DATA de la función getHeroes 
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
