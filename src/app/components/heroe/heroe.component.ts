5494dfb8f7c4686e13b9f3d17dcc012805a29a29
Jamilett <jam.letsam93@gmail.com>
2021-11-01 18:28:19 -0600

    Se agregan comentarios respecto a activatedRoute

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -11,8 +11,10 @@ export class HeroeComponent {
   heroe: any = {};
 
   constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService) {
+    // activatedRoute: proporciona acceso a la información de la ruta asociada a un componente cargado.
+    // .params "observa" los parámetros de la matriz dentro del alcance de la ruta asociada.
     this.activatedRoute.params.subscribe(params => {
-      console.log(params); // Params de la URL
+      console.log(params); // Params de la URL { regresan en un obj }
       this.heroe = this._heroesService.getHeroe(params['id']); // El id es porque en las rutas así lo nombramos
       console.log(this.heroe);
     })
